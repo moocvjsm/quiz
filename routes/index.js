f0f66259dbac3f752ee73f81a14c684cffec5e49
moocvjsm <osobukorn@gmail.com>
2015-07-30 00:45:28 +0200

    Crear Preguntas

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,8 @@ router.param('quizId', quizController.load); // autoload :quizId
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
 
 /*router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);*/
