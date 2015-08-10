81e8f38fb795a444d754019d783bfc3afa5b522e
moocvjsm <osobukorn@gmail.com>
2015-08-11 01:17:57 +0200

    Moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 });
 
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -32,6 +33,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 // rutas para añadir comentarios o ver
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 /*router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);*/
